72408a8411140125402ce12c46f5b1bd83f22c81
MaryR-code <m.romanovskaia@gmail.com>
2021-01-10 13:01:15 +0200

    FlightDuration 1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,14 +15,14 @@ public class Main {
         var departureTime = InputUtils.askTime("Enter time of departure");
 
         var departureZoneDateTime = ZonedDateTime.of(departureDate, departureTime, departureZone);
-        System.out.println("Departure date/time/zone: " + datetimeFormatter.format(departureZoneDateTime));
+    //    System.out.println("Departure date/time/zone: " + datetimeFormatter.format(departureZoneDateTime));
 
         var destinationZone = InputUtils.askZone("Enter timezone of destination");
         var destinationDate = InputUtils.askDate("Enter date of destination");
         var destinationTime = InputUtils.askTime("Enter time of destination");
 
         var destinationZoneDateTime = ZonedDateTime.of(destinationDate, destinationTime, destinationZone);
-        System.out.println("Destination date/time/zone: " + datetimeFormatter.format(destinationZoneDateTime));
+    //    System.out.println("Destination date/time/zone: " + datetimeFormatter.format(destinationZoneDateTime));
 
         Duration duration = Duration.between(departureZoneDateTime, destinationZoneDateTime);
         System.out.println("Duration of your flight: " + duration.toHours() + " hours");
